a8d680516b88670ae1a9b615951483b494b79a27
zaid03 <terguyzaid@gmail.com>
2025-07-27 02:09:48 +0100

    Add Landing Page and Authentication Components
    - Implemented Auth component with basic structure.
    - Created LandingPage component with navigation, header, and footer.
    - Added CSS styles for LandingPage and its elements.
    - Included assets for Spotify logos and demo images.
    - Set up form handling for user input in the contact section.
    - Established routing links for navigation between features and contact pages.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import LandingPage from './components/LandingPageComponent';
+import Auth from './components/AuthComponent';
+import '@fontsource/poppins';
+import '@fontsource/montserrat';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> zaid.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/Auth" element={<Auth />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/LandingPageComponent/LandingPage.jsx
@@ -0,0 +1,150 @@
+import React, { useEffect } from 'react';
+import {Link} from 'react-router-dom';
+import './LandingPage.css';
+import logo from '../../assets/logo.png';
+import spotify from '../../assets/spotify.png';
+import demo from '../../assets/demo.png';
+import white from '../../assets/white-spotify.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
+import { faMusic, faUser, faCompactDisc, faRobot } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+
+function LandingPage() {
+    useEffect(() =>{
+        console.log("am loaded");
+    }, []);
+
+    const [formData, setformData] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const handlechange = (e) => {
+        setformData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+        //here goes the logic for submitting (late8)
+    }
+
+    return (
+        <div className='container'>
+            <div className='header'>
+                <div className='nav-bar'>
+                    <div className='logo-name'>
+                        <img src={logo} alt='Lunafy logo' />
+                        <span className='name'>Lunafy</span>
+                    </div>
+                    <div className='navigation'>
+                        <Link to='/'>Features</Link>
+                        <Link to='/Contact'>Contact us</Link>
+                    </div>
+                    <div className='login'>
+                        <Link to='/Login' className='login-btn'><img src={spotify} alt='Spotify icon by Icons8' className='spotify'/> Login with spotify</Link>
+                    </div>
+                </div>
+
+                <div className='header-content'>
+                    <div className='left'>
+                        <h1>Spotify stats</h1>
+                        <h2 className='tailor'>Tailored for you</h2>
+                        <p>Connect your Spotify account and get detailed insights about your listening habits.</p>
+                        <Link to='/Login' className='login-btn'><img src={white} alt='Spotify icon by Icons8' className='spotify'/> Login with spotify</Link>
+                        <p className='conditions'>By clicking Login, you agree to our <Link to='/terms' className='terms'>Terms of Services</Link> and <Link to='/privacy' className='privacy'>Privacy Policy</Link> </p>
+                    </div>
+                    
+                    <div className='right'>
+                        <img src={demo} alt='screenshot or video from the website' />
+                    </div>
+                </div>
+            </div>
+
+
+            <div className='discover'>
+                <h3>Uncover Who You Are — One Song At A Time</h3>
+
+                <div className='cards-container'>
+                    <div className='card'>
+                        <FontAwesomeIcon icon={faMusic} />
+                        <h1>Top track</h1>
+                        <p>Discover your most played songs across diffrent time periods</p>
+                    </div>
+                    <div className='card'>
+                        <FontAwesomeIcon icon={faUser} />
+                        <h1>Top artists</h1>
+                        <p>Discover what artists you listen the most</p>
+                    </div>
+                    <div className='card'>
+                        <FontAwesomeIcon icon={faCompactDisc} />
+                        <h1>Top genres</h1>
+                        <p>Discover how you favourite genre changes depending on your mood</p>
+                    </div>
+                    <div className='card'>
+                        <FontAwesomeIcon icon={faRobot} />
+                        <h1>Music Personality</h1>
+                        <p>Discover your personality through music, powered by smart AI analysis.</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div className='contact'>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type='text'
+                        name='name'
+                        value={formData.name}
+                        onChange={handlechange}
+                        placeholder='Enter your name'
+                    />
+                    <input 
+                        type='email'
+                        name='email'
+                        value={formData.email}
+                        onChange={handlechange}
+                        placeholder='Enter your email'
+                    />
+                    <textarea 
+                        name='message'
+                        value={formData.message}
+                        onChange={handlechange}
+                        placeholder='Enter your message'
+                    />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+
+            <div className='footer'>
+                <div className='nav'>
+                    <Link to='/'>Features</Link>
+                    <Link to='/Contact'>Contact us</Link>
+                </div>
+
+                <Link to='/Login' className='login-btn'><img src={spotify} alt='Spotify icon by Icons8' className='spotify'/> Login with spotify</Link>
+
+                <div className='social'>
+                    <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faGithub} />
+                    </a>
+                     <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faLinkedin} />
+                    </a>
+                </div>
+                <div className='misc'>
+                    <p>© 2025 Lunafy — Crafted with ♫ and ☕</p>
+                    <p>Made with ❤️ in Morocco</p>
+                </div>
+                
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage;
